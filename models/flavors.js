24fea7693db996f0bf01e3a55eda6d6e5c190936
Agustin <agustinborsaumai@gmail.com>
2023-11-09 23:36:53 -0300

    adding products and flavors schema

---
+++ models/flavors.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+
+const flavorSchema = new Schema ({
+  name: { type: String },
+  image: { type: String },
+  description: { type: String },
+  apto: [{
+    vegano: {type: Boolean},
+    celiaco: {type: Boolean}
+  }]
+});
+
+export default mongoose.model("Flavor", flavorSchema, "Flavors");
